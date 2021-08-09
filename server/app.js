ea9e13f9ad57cba8c184b1c6a47a5bba10295b93
yeoularu <yeoularu@gmail.com>
2021-08-09 17:08:40 +0900

    server papago api success

--- server/app.js
+++ server/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
-const app = express();
-const query = "번역할 문장을 입력하세요.";
 
 dotenv.config({
   path: path.resolve(
@@ -11,18 +9,21 @@ dotenv.config({
   ),
 });
 
+const app = express();
+let query = "express";
+
 app.get("/translate", (req, res) => {
   const api_url = "https://openapi.naver.com/v1/papago/n2mt";
   const request = require("request");
   const options = {
     url: api_url,
-    form: { source: "ko", target: "en", text: query },
+    form: { source: "en", target: "ko", text: query },
     headers: {
       "X-Naver-Client-Id": process.env.client_id,
       "X-Naver-Client-Secret": process.env.client_secret,
     },
   };
-  request.post(options, function (error, response, body) {
+  request.post(options, (error, response, body) => {
     if (!error && response.statusCode == 200) {
       res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
       res.end(body);
@@ -32,6 +33,6 @@ app.get("/translate", (req, res) => {
     }
   });
 });
-app.listen(3000, function () {
-  console.log("http://127.0.0.1:3000/translate app listening on port 3000!");
+app.listen(3000, () => {
+  console.log("http://localhost:3000/translate app listening on port 3000!");
 });
