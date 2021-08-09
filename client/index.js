c917443cbe78e428497cd254f4f5c4b785638f21
yeoularu <yeoularu@gmail.com>
2021-08-10 02:10:37 +0900

    console.log translated text

--- client/index.js
+++ client/index.js
@@ -1,19 +1,24 @@
-let selectedText = "";
+
+let selectedText = "",
+  translatedText = "";
 
 document.onmouseup = () => {
   if (document.getSelection().toString().length > 0) {
-    selectedText = document.getSelection().toString()
-    
+    selectedText = document.getSelection().toString();
+
     console.log(selectedText);
 
     fetch("http://127.0.0.1:3000/translate", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        text: selectedText
+        text: selectedText,
       }),
     })
       .then((res) => res.json())
-      .then((res) => console.log(res));
+      .then((res) => {
+        translatedText = res.message.result.translatedText;
+        console.log(translatedText);
+      });
   }
 };
